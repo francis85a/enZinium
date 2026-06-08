2e4d37a88a6187a949aa3640e6c17a6c9276c7e7
francis85a <franperezcid85@gmail.com>
2026-06-08 13:42:16 +0200

    test(Test): Añadir casos test para Address

---
+++ src/test/java/edu/teamrocket/enZinium/AddressTest.java
@@ -0,0 +1,41 @@
+package edu.teamrocket.enZinium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import org.junit.jupiter.api.Test;
+
+public class AddressTest {
+
+    /**
+     * Test SK y PK
+     */
+    @Test
+    public void constructor_test() {
+        Address address = new Address();
+        assertNotNull(address);
+        assertEquals(0d, address.getBalance(), 0d);
+    }
+
+    @Test
+    public void generate_key_pair_test() {
+        Address address = new Address();
+        assertNotNull(address);
+        address.generateKeyPair();
+        assertNotNull(address.getPK());
+        assertNotNull(address.isSKpresent());
+    }
+
+    @Test
+    public void transferEZI_test() {
+
+        Address rick = new Address();
+        rick.generateKeyPair();
+
+        rick.transferEZI(20d);
+        rick.transferEZI(20d);
+
+        assertEquals(40d, rick.getBalance(), 0d);
+    }
+}
